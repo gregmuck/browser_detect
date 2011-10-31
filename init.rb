68b44b024ce32410baf798fd9a1acc49744e3350
gregmuck <gregm@hookinteractive.com>
2011-10-31 15:30:23 -0400

    Trying another approach to getting initialized for rails 2.3

--- init.rb
+++ init.rb
@@ -1,5 +1,10 @@
 if Rails::VERSION::MAJOR == 2
-  ApplicationController.send(:include, BrowserDetect)
+  require 'browser_detect'
+
+  class ActionController::Base
+    include BrowserDetect
+  end
+#    ApplicationController.send(:include, BrowserDetect)
 else
   require "railtie" if defined?(Rails) && defined?(Rails::Railtie)
 end
