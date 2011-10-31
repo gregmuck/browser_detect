6b044b5e3da7ab1bfb764261b7f96d5efe5124b7
gregmuck <gregm@hookinteractive.com>
2011-10-31 16:06:43 -0400

    Rolled back changes to plugin init in favour of initializer within Rails itself

--- init.rb
+++ init.rb
@@ -1,10 +1,5 @@
 if Rails::VERSION::MAJOR == 2
-  require 'browser_detect'
-
-  class ActionController::Base
-    include BrowserDetect
-  end
-#    ApplicationController.send(:include, BrowserDetect)
+    ActionView::Base.send(:include, BrowserDetect)
 else
   require "railtie" if defined?(Rails) && defined?(Rails::Railtie)
 end
